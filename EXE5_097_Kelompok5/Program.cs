e8897493e63120c26489097417da61935c197464
ZaqyMunif <zaqymunif17@gmail.com>
2022-12-23 16:14:59 +0700

    add class node

--- EXE5_097_Kelompok5/Program.cs
+++ EXE5_097_Kelompok5/Program.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace EXE5_097_Kelompok5
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
-        {
-        }
+        public string element;
+        public string name;
+        public Node next;
     }
 }
